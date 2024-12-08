d1b4bef3389f9982f6f9f905f000f4d2f28fa1b2
Holesmak <holesmakl@gmail.com>
2024-12-08 14:04:03 +0200

    Reworking design for auth screen to compose

--- app/src/main/java/com/github/mkikets99/lntumessengerproject/ui/theme/Type.kt
+++ app/src/main/java/com/github/mkikets99/lntumessengerproject/ui/theme/Type.kt
@@ -1,11 +1,23 @@
 package com.github.mkikets99.lntumessengerproject.ui.theme
 
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.github.mkikets99.lntumessengerproject.R
 
+val DeliushSwashCapsFont = Font(
+    resId = R.font.delius_swash_caps_regular,
+    weight = FontWeight.Normal
+)
+
+val DeliushSwashCaps = FontFamily(
+    DeliushSwashCapsFont
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -14,15 +26,17 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    titleLarge = TextStyle(
+        fontFamily = DeliushSwashCaps,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.sp
+    )
+
+
+     /*
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
