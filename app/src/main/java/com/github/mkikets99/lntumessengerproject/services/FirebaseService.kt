088c1ad6bcbbf3c10e0b18b9e0926246ec2c3ea1
Mykola Kikets <mykola.kikets@gmail.com>
2024-12-08 10:03:29 +0200

    Main Activity to FirebaseService.Auth Adapt

--- app/src/main/java/com/github/mkikets99/lntumessengerproject/services/FirebaseService.kt
+++ app/src/main/java/com/github/mkikets99/lntumessengerproject/services/FirebaseService.kt
@@ -1,13 +1,13 @@
 package com.github.mkikets99.lntumessengerproject.services
 
 import android.content.Context
-import androidx.core.util.Function
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 import com.google.firebase.initialize
-import java.util.concurrent.Callable
 
 class FirebaseService {
 
@@ -15,9 +15,6 @@ class FirebaseService {
 
     companion object {
         val instance = FirebaseService()
-    }
-
-    private constructor(){
     }
 
     fun init(context: Context){
@@ -48,5 +45,18 @@ class FirebaseService {
         database.collection(collection).document(document).addSnapshotListener{ snap,e->
             callback(snap,e)
         }
+    }
+
+    fun requestAuth(callback : ActivityResultLauncher<Intent>){
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+
+        callback.launch(signInIntent)
     }
 }
