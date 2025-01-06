d1c18d241a804fc397b7896e46ff8dc88a9c36a1
Holesmak <holesmakl@gmail.com>
2025-01-06 19:37:09 +0200

    Changed secondary theme color

--- app/src/main/java/com/github/mkikets99/lntumessengerproject/ui/theme/Color.kt
+++ app/src/main/java/com/github/mkikets99/lntumessengerproject/ui/theme/Color.kt
@@ -7,9 +7,9 @@ val PurpleGrey80 = Color(0xFFCCC2DC) //CCC2DC
 val Pink80 = Color(0xFFEFB8C8) //EFB8C8
 
 val Blue = Color(0xFF49AFDB) //6650a4
-val BlueSecondary = Color(0xFF00DB8F) //625b71
+val BlueSecondary = Color(0xFF000000) //625b71 ///00DB8F
 val BlueTrinary = Color(0xFF000EDB) //7D5260
 
 val BlueDark = Color(0xFF264ca0)
-val BlueDarkSecondary = Color(0xFF01a288)
+val BlueDarkSecondary = Color(0xFFFFFFFF) ///01a288
 val BlueDarkTrinary = Color(0xFF01ec88)
